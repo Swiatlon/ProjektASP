18be733d3742d5474d5e46026a4008f630c2e2b8
Swiatlon <przemyslaw.swiatlon@gmail.com>
2024-02-05 22:27:40 +0100

    First part with working basic

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+        public class AppDbContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        private string DbPath { get; set; }
+        public AppDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "products.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<ProductEntity>().HasData(
+                new ProductEntity()
+                {
+                    Id = 1,
+                    Name = "Product 1",
+                    Price = 10.99m,
+                    Producer = "Producer 1",
+                    ProductionDate = new DateTime(2020, 10, 10),
+                    Description = "Product 1 description",
+                },
+                new ProductEntity()
+                {
+                    Id = 2,
+                    Name = "Product 2",
+                    Price = 20.99m,
+                    Producer = "Producer 2",
+                    ProductionDate = new DateTime(2023, 10, 10),
+                    Description = "Product 2 description",
+                }
+            );
+        }
+    }
+}
---
+++ Data/Entities/ProductEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+    [Table("Products")]
+    public class ProductEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Producer { get; set; }
+
+        [Required]
+        public DateTime ProductionDate { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
